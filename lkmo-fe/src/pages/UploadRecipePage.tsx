702f4e78b00cc5ca0750fdc42289261cf59ab624
Mario Fransiskus Sitepu <stinart123@GMAIL.COM>
2025-11-09 09:54:19 +0700

    fix : delete text info in another tools

--- lkmo-fe/src/pages/UploadRecipePage.tsx
+++ lkmo-fe/src/pages/UploadRecipePage.tsx
@@ -383,7 +383,7 @@ export default function UploadRecipePage() {
                 id="other-equipment"
                 value={formData.otherEquipment}
                 onChange={(e) => setFormData({ ...formData, otherEquipment: e.target.value })}
-                placeholder="Pisahkan dengan koma (contoh: pisau, talenan)"
+                placeholder="Masukkan peralatan lainnya (contoh: pisau)"
                 className="w-full p-3 border border-gray-300 rounded-md focus:ring-green-500 focus:border-green-500"
               />
             </div>
